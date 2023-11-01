606e5c88938462aa1701c556b6db2079b5e07dd5
EgorLaptenok <egorlaptenok@gmail.com>
2023-11-01 16:55:57 +0300

    add lesson9 HW

---
+++ src/lesson9/exercise1/WrongLoginException.java
@@ -0,0 +1,12 @@
+package lesson9.exercise1;
+
+public class WrongLoginException extends Exception {
+
+    public WrongLoginException() {
+        super();
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
